67ac16bf5bcfd8560766b509bfc63c02dc19cd66
Tyler Hancock <darklime@live.ca>
2016-07-28 15:56:20 -0600

    Created the basic feature system.

--- src/main/java/net/darkhax/ctweaks/CTweaks.java
+++ src/main/java/net/darkhax/ctweaks/CTweaks.java
@@ -1,8 +1,13 @@
 package net.darkhax.ctweaks;
 
+import net.darkhax.ctweaks.features.Feature;
+import net.darkhax.ctweaks.features.FeatureManager;
 import net.darkhax.ctweaks.lib.Constants;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Constants.MOD_ID, name = Constants.MOD_NAME, version = Constants.VERSION_NUMBER, acceptedMinecraftVersions = "[1.9.4,1.10.2]")
@@ -14,5 +19,22 @@ public class CTweaks {
     @EventHandler
     public void preInit (FMLPreInitializationEvent event) {
         
+        FeatureManager.FEATURES.forEach(Feature::onPreInit);
+        
+        for (final Feature feature : FeatureManager.FEATURES)
+            if (feature.usesEvents())
+                MinecraftForge.EVENT_BUS.register(feature);
+    }
+    
+    @EventHandler
+    public void init (FMLInitializationEvent event) {
+        
+        FeatureManager.FEATURES.forEach(Feature::onInit);
+    }
+    
+    @EventHandler
+    public void postInit (FMLPostInitializationEvent event) {
+        
+        FeatureManager.FEATURES.forEach(Feature::onPostInit);
     }
 }
---
+++ src/main/java/net/darkhax/ctweaks/features/FeatureManager.java
@@ -0,0 +1,48 @@
+package net.darkhax.ctweaks.features;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.darkhax.ctweaks.handler.ConfigurationHandler;
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
+import net.minecraftforge.fml.relauncher.Side;
+
+public class FeatureManager {
+    
+    /**
+     * List of all registered features.
+     */
+    public static final List<Feature> FEATURES = new ArrayList<>();
+    
+    /**
+     * This method is called before any mods have had a chance to initialize. Constructors
+     * should take care not to reference any actual game code.
+     */
+    public static void initFeatures () {
+        
+        if (FMLLaunchHandler.side() == Side.CLIENT) {
+            
+        }
+        
+    }
+    
+    /**
+     * Registers a new feature with the feature manager. This will automatically create an
+     * entry in the configuration file to enable/disable this feature. If the feature has been
+     * disabled, it will not be registered. This will also handle event bus subscriptions.
+     *
+     * @param feature The feature being registered.
+     * @param name The name of the feature.
+     * @param description A short description of the feature.
+     */
+    private static void registerFeature (Feature feature, String name, String description) {
+        
+        feature.enabled = ConfigurationHandler.isFeatureEnabled(feature, name, description);
+        
+        if (feature.enabled) {
+            
+            feature.configName = name.toLowerCase().replace(' ', '_');
+            FEATURES.add(feature);
+        }
+    }
+}
