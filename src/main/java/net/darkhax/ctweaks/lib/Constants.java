5b986f8769a8f828b347493e55aa04a93fcf0c74
Tyler Hancock <darklime@live.ca>
2016-08-07 12:04:11 -0600

    Added a logger

--- src/main/java/net/darkhax/ctweaks/lib/Constants.java
+++ src/main/java/net/darkhax/ctweaks/lib/Constants.java
@@ -1,8 +1,12 @@
 package net.darkhax.ctweaks.lib;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Constants {
     
     public static final String MOD_ID = "ctweaks";
     public static final String MOD_NAME = "Custom Tweaks";
     public static final String VERSION_NUMBER = "1.0.0.0";
+    public static final Logger LOG = LogManager.getLogger(MOD_NAME);
 }
